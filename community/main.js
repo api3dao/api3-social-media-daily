7ccb7f6548d9fdf306046634bc4f258ad77c9415
Warren Anderson <3522283+wkande@users.noreply.github.com>
2026-02-04 06:48:44 -0700

    2026feb03 community telegram (#5)
    * add run community slack thread
    
    * change output message
    
    * change block header label
    
    * new community slack group
    
    * change startup output
    
    * change begin message output for each run cycle
    
    * added message counts output for visual verification

--- community/main.js
+++ community/main.js
@@ -1,14 +1,14 @@
 const { getXQueryRuntimeDttm } = require("../utils/utc");
-const { postTweets } = require("./tweets");
+const { postMessages } = require("./community");
 
 /**
  * Called by the cron job from app.js once a day
  */
 async function run() {
-  console.log("\n----- Running the X report -----");
+  console.log("\n----- Running the community report -----");
   try {
     console.log(">>>", await getXQueryRuntimeDttm());
-    await postTweets();
+    await postMessages();
   } catch (error) {
     console.error(error);
   }
